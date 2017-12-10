5ede3c9811a28da6ec6a07a98e163240c052af4e
sourabhpc <sourabhpanadare@yahoo.co.in>
2017-12-10 18:54:36 +0530

    Change of Folder

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # cordova-ludo-app
+
+# Cordova Installation procedure
+
+   sudo npm install -g cordova
+
+   cordova create LudoGame com.sourabh.lodogame LudoGame
+
+   cd LudoGame
+
+   cordova plugin add cordova-plugin-device
+   cordova plugin add cordova-plugin-network-information
+   cordova plugin add cordova-plugin-media
+   cordova plugin add cordova-plugin-splashscreen
+
+   cordova platform add android
+   cordova platform add ios
+
---
+++ assets/www/js/admob.js
@@ -0,0 +1,31 @@
+function adSetter(){
+// alert(navigator.userAgent);
+var admobid = {};
+// select the right Ad Id according to platform
+if( /(android)/i.test(navigator.userAgent) ) { 
+    admobid = { // for Android
+        banner: 'ca-app-pub-4567456594388265/6277666237',
+        interstitial: 'ca-app-pub-4567456594388265/1707865835'
+    };
+} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+    admobid = { // for iOS
+        banner: 'ca-app-pub-6869992474017983/4806197152',
+        interstitial: 'ca-app-pub-6869992474017983/7563979554'
+    };
+} else {
+    admobid = { // for Windows Phone
+        banner: 'ca-app-pub-6869992474017983/8878394753',
+        interstitial: 'ca-app-pub-6869992474017983/1355127956'
+    };
+}
+
+if(window.AdMob) AdMob.createBanner( {
+    adId:admobid.banner, 
+    position:AdMob.AD_POSITION.BOTTOM_CENTER, 
+    autoShow:true} );
+
+ if(window.AdMob) AdMob.prepareInterstitial( {adId:admobid.interstitial, autoShow:false} );
+
+
+}
+ 
